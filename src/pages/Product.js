87e50e94ea8cdadfc4912dcd10a91d9d4c647ddc
althafAhd <althafofficial07@gmail.com>
2022-10-22 14:38:46 +0530

    thumbnail image preview feature ready

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,22 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./product.css";
 // import { useParams } from "react-router-dom";
-import MainImg from "../images/image-product-1.jpg";
-import ImgThumbanil from "../images/image-product-1-thumbnail.jpg";
 
+const images = [
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428070/image-product-1_xqdxbx.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428075/image-product-1-thumbnail_vkcet8.jpg",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428098/image-product-2_jlsvyk.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428113/image-product-2-thumbnail_lw5kfu.jpg",
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428134/image-product-3_hd30kn.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428133/image-product-3-thumbnail_zkodmk.jpg",
+  },
+  {
+    id: 4,
+    img: "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428178/image-product-4_ptnnmn.jpg",
+    thumbnail:
+      "https://res.cloudinary.com/davg6e0yh/image/upload/v1666428178/image-product-4-thumbnail_bazeat.jpg",
+  },
+];
 const Product = () => {
   // const { id } = useParams();
+  const [mainImgId, setMainImgId] = useState(0);
   return (
     <div className="product">
       <div className="imageGallery">
         <div className="imagePreview">
-          <img src={MainImg} alt="mainImage" />
+          <img src={images[mainImgId].img} alt="mainImage" />
         </div>
         <div className="thumbnail">
-          <img src={ImgThumbanil} alt="thumbnail" />
-          <img src={ImgThumbanil} alt="thumbnail" />
-          <img src={ImgThumbanil} alt="thumbnail" />
-          <img src={ImgThumbanil} alt="thumbnail" />
+          {images.map((image, index) => {
+            return (
+              <img
+                key={image.id}
+                onMouseOver={() => {
+                  console.log("i am working");
+                  new Image().src = image.img;
+                }}
+                onClick={() => setMainImgId(index)}
+                src={image.thumbnail}
+                alt="thumbnail"
+                style={{
+                  filter: `${
+                    mainImgId === index ? "grayscale(80%)" : "grayscale(0%)"
+                  }`,
+                }}
+              />
+            );
+          })}
         </div>
       </div>
       <div className="productData">
