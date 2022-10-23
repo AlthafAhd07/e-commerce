c2cf18020d49cc98891fa102db0cf5357e784f39
althafAhd <althafofficial07@gmail.com>
2022-10-23 19:13:33 +0530

    cart checkout section layerout finished

--- src/components/userCart/Right.js
+++ src/components/userCart/Right.js
@@ -1,7 +1,44 @@
 import React from "react";
 import "./style.css";
 const Right = () => {
-  return <div className="cart__right">Right</div>;
+  return (
+    <div className="cart__right">
+      <h1 className="cart__summaryTitle">Order Summary</h1>
+      <div className="cart__other">
+        <div className="ItemCountPrice">
+          <span>3 ITEMS</span> <span>$457</span>
+        </div>
+        <div className="ItemShippingOption">
+          <p>SHIPPING</p>
+          <select>
+            <option value="">Standart delivery - $5</option>
+            <option value="">Fast delivery - $15</option>
+          </select>
+        </div>
+        <div className="item__promoCode">
+          <p>PROMO CODE</p>
+          <input type="text" placeholder="Enter your code" />
+          <button>APPLY</button>
+        </div>
+      </div>
+      <hr className="horizontalLine" />
+      <div className="cart__total">
+        <div className="ItemCountPrice">
+          <span>SUBTOTAL</span>
+          <span>$300</span>
+        </div>
+        <div className="ItemCountPrice">
+          <span>DELIVERY</span>
+          <span>$3</span>
+        </div>
+        <div className="ItemCountPrice">
+          <span>TOTAL</span>
+          <span>$303</span>
+        </div>
+        <button>CHECKOUT</button>
+      </div>
+    </div>
+  );
 };
 
 export default Right;
